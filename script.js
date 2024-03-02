c6e4c48c9a4c522ee8cd8e673d5bb9973eafe4ef
Ajai-Sharan <ajaisharan2020@gmail.com>
2024-03-02 16:50:59 +0530

    Added files of Nostra ecommerce

---
+++ script.js
@@ -0,0 +1,11 @@
+var side_navBar = document.querySelector(".side_navBar");
+
+function open_sideBar() {
+    side_navBar.style.left = 0;
+}
+
+function close_sideBar(){
+    side_navBar.style.left = "-70%";
+}
+
+
